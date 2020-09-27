7c4818b71334cc57adb5f5ad5fc263aa866c3ee4
Alberto cime <orland_998@hotmail.com>
2020-09-27 00:43:18 -0500

    agregado del modelo alarma js

---
+++ app/models/alarma.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//Creating a new token Schema
+const AlertaSchema = new Schema({
+    id_token: { type: Schema.Types.ObjectId, ref: 'Token' },
+    ip_address: String,
+    hostname: String,
+    mac_addres: String,
+    hora_apertura: { type: Date, default: Date.now },
+    read: { type: Boolean, default: false }
+}, {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+});
+let Model = mongoose.model('Alarma', AlertaSchema);
+
+
+module.exports = Model
